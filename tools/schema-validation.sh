fbb06f1eccae80451f7df82ddb2722696cc6408e
René Fehlow <rene.fehlow@web.de>
2021-11-22 21:26:25 +0100

    Finished required fields

--- tools/schema-validation.sh
+++ tools/schema-validation.sh
@@ -3,4 +3,4 @@ cd "$(dirname "$0")"
 pip install jsonschema
 cd ..
 cd v1.0.0
-jsonschema -i open-brew-1-0-0.example.json open-brew-1-0-0.schema.json
+jsonschema -i open-brew-1-0-0.example.min.json open-brew-1-0-0.schema.json
